4a7bfd10fcd0da2311a28d686c3e9f6ae9d5ca7b
irreaaa <irispbmink@gmail.com>
2025-02-26 18:51:26 +0300

    RecPass SignUp commit

--- app/src/main/java/com/example/myapplication/ui/screen/SignIn/RecoverPasswordScrn.kt
+++ app/src/main/java/com/example/myapplication/ui/screen/SignIn/RecoverPasswordScrn.kt
@@ -1,8 +1,5 @@
 package com.example.myapplication.ui.screen.SignIn
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -10,35 +7,20 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myapplication.R
 import com.example.myapplication.ui.screen.SignIn.component.AuthButton
 import com.example.myapplication.ui.screen.SignIn.component.AuthTextField
 import com.example.myapplication.ui.screen.SignIn.component.TitleWithSubtitleText
-import com.example.myapplication.ui.theme.MatuleTheme
 
 @Composable
 fun RecoverPasswordScrn(){
